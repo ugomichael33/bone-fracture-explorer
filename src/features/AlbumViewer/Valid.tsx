ee0b2df8167cb08f412e6dc48ac617cd1f51031c
michael onyekanma <michael@nebula.io>
2024-02-29 16:38:19 +0100

    fixed bug

--- src/features/AlbumViewer/Valid.tsx
+++ src/features/AlbumViewer/Valid.tsx
@@ -1,4 +1,4 @@
-import React. { useState } from "react";
+import React, { useState } from "react";
 
 import PopupContainer from "@/components/PopupContainer";
 import ImagePaginator, { fetchPageData } from "@/components/ImagePaginator";
